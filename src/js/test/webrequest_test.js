98243586b17a6ed6853c9b231754a478b6d01629
Blake Griffith <cowlicks@riseup.net>
2017-10-30 23:46:43 -0700

    Add webrequest.js and tests

---
+++ src/js/test/webrequest_test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const assert = require('chai').assert,
+  {WebRequest} = require('../webrequest'),
+  {Tabs} = require('../tabs');
+
+
+const main_details = {frameId: 0, url: 'https://google.com/', tabId: 1, parentFrameId: -1, type: 'main_frame'},
+  sub_details = {frameId: 1, url: 'about:blank', tabId: 1, parentFrameId: 0, type: 'sub_frame'};
+
+describe('webrequest.js', function() {
+  describe('WebRequest', function() {
+    describe('#onBeforeRequest', function() {
+      it('adds frames', function() {
+        let tabs = new Tabs(),
+          wr = new WebRequest(tabs, {});
+
+        wr.onBeforeRequest(main_details);
+        assert.equal(tabs.getTabUrl(main_details.tabId), main_details.url);
+
+        wr.onBeforeRequest(sub_details);
+        assert.equal(tabs.getFrameUrl(sub_details.tabId, sub_details.frameId), sub_details.url);
+      });
+    });
+  });
+});
