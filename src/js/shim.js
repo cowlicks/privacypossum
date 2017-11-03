a6ee2ecf7650c6df471ccc0ed0e04d2e7c1fe58a
Blake Griffith <cowlicks@riseup.net>
2017-11-03 12:51:25 -0700

    Add onBeforRequest to shim

--- src/js/shim.js
+++ src/js/shim.js
@@ -30,11 +30,19 @@ try {
   sendMessage_ = fm.sendMessage.bind(fm);
 }
 
+let onBeforeRequest;
+try {
+  onBeforeRequest = chrome.webRequest.onBeforeRequest;
+} catch (e) {
+  onBeforeRequest = new FakeMessages();
+}
+
 Object.assign(exports, {
   URL: url_,
   Disk: disk_,
   onMessage: onMessage_,
   sendMessage: sendMessage_,
+  onBeforeRequest,
 });
 
 })(typeof exports == 'undefined' ? require.scopes.shim = {} : exports);
