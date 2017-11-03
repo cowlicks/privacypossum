8161380c7d72ab4c5e993b901050450500c716d7
Blake Griffith <cowlicks@riseup.net>
2017-11-03 12:51:58 -0700

    eslint possum tests

--- src/js/test/possum_test.js
+++ src/js/test/possum_test.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const assert = require('chai').assert,
-  {Possum} = require('../possum'),
-  {Mock} = require('./testing_utils');
+  {Possum} = require('../possum');
 
 describe('possum.js', function() {
   it('constructs', function() {
     let possum = new Possum();
+    assert.ok(possum);
   });
 });
