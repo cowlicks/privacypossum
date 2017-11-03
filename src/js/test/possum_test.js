5be852719d0c9341878b0c006377589b3bce710d
Blake Griffith <cowlicks@riseup.net>
2017-11-03 14:50:49 -0700

    complete fingerprinting integration test

--- src/js/test/possum_test.js
+++ src/js/test/possum_test.js
@@ -1,11 +1,27 @@
 "use strict";
 
 const assert = require('chai').assert,
+  constants = require('../constants'),
+  {onBeforeRequest, sendMessage} = require('../shim'),
+  {details} = require('./testing_utils'),
   {Possum} = require('../possum');
 
 describe('possum.js', function() {
-  it('constructs', function() {
+  it('constructs', async function() {
     let possum = new Possum();
-    assert.ok(possum);
+
+    // load a page, with a script
+    onBeforeRequest.sendMessage(details.main_frame);
+    onBeforeRequest.sendMessage(details.script);
+
+    // page see's fingerprinting and sends message
+    await sendMessage(
+      {type: constants.FINGERPRINTING, url: details.script.url},
+      details.main_frame.toSender()
+    );
+
+    // another request for the fingerprinting script is made
+    let result = possum.webRequest.onBeforeRequest(details.script);
+    assert.deepEqual(result, constants.CANCEL);
   });
 });
