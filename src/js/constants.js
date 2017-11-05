eecc1ae5daae2e6260c88fad78b023dabaf185cf
Blake Griffith <cowlicks@riseup.net>
2017-11-05 14:01:48 -0800

    Add details.type's to constants.js

--- src/js/constants.js
+++ src/js/constants.js
@@ -10,11 +10,16 @@ const NO_ACTION = {},
   CANCEL = {cancel: true};
 
 // suffix of main_frame & sub_frame
+const TYPES = {
+  main_frame : 'main_frame',
+  sub_frame : 'sub_frame',
+};
+
 const FRAME_END = '_frame';
 
 // reasons
 const FINGERPRINTING = 'fingerprinting';
 
-Object.assign(exports, {NO_ACTION, CANCEL, FRAME_END, FINGERPRINTING, DISK_NAME});
+Object.assign(exports, {NO_ACTION, CANCEL, TYPES, FRAME_END, FINGERPRINTING, DISK_NAME});
 
 })(typeof exports == 'undefined' ? require.scopes.constants = {} : exports);
