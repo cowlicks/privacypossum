fa2134d86a261d056e946d76fede6740b85f8cbd
Blake Griffith <cowlicks@riseup.net>
2017-12-01 13:51:33 -0800

    Break out request handler reasons.

--- src/js/reasons.js
+++ src/js/reasons.js
@@ -12,33 +12,44 @@ function setResponse(response, shortCircuit) {
 
 const tabDeactivate = new Action({response: NO_ACTION, reason: TAB_DEACTIVATE});
 
-function makeReasonMap() {
-  return new Map([
-    [FINGERPRINTING, setResponse(CANCEL, false)],
-    [USER_URL_DEACTIVATE, setResponse(NO_ACTION, false)],
-    [TAB_DEACTIVATE, setResponse(NO_ACTION, true)],
-    [USER_HOST_DEACTIVATE, (details, {tabs}) => {
-      details.shortCircuit = true;
-      details.response = NO_ACTION;
-      tabs.getTab(details.tabId).action = tabDeactivate;
-    }],
-  ]);
-}
+const reasons = [
+  [FINGERPRINTING, setResponse(CANCEL, false)],
+  [USER_URL_DEACTIVATE, setResponse(NO_ACTION, false)],
+  [TAB_DEACTIVATE, setResponse(NO_ACTION, true)],
+  [USER_HOST_DEACTIVATE, (details, {tabs}) => {
+    details.shortCircuit = true;
+    details.response = NO_ACTION;
+    tabs.getTab(details.tabId).action = tabDeactivate;
+  }],
+];
 
 // todo wrap handler requests to assure main_frame's are not blocked.
 class Handler {
   constructor(tabs, store) {
     Object.assign(this, {tabs, store});
-    this.reasonMap = makeReasonMap();
+    this.funcs = new Map();
+    reasons.forEach(([name, func]) => {
+      this.addReason(new Reason(name, {requestHandler: func}));
+    });
   }
 
   handleRequest(obj, details) {
     if (obj.hasOwnProperty('action')) {
-      this.reasonMap.get(obj.action.reason)(details, {tabs: this.tabs, store: this.store});
+      this.funcs.get(obj.action.reason)(details, {tabs: this.tabs, store: this.store});
     }
+  }
+
+  addReason(reason) {
+    this.funcs.set(reason.name, reason.requestHandler);
   }
 }
 
-Object.assign(exports, {Handler, tabDeactivate});
+class Reason {
+  constructor(name, {messageHandler, requestHandler}) {
+    Object.assign(this, {name, messageHandler, requestHandler});
+  }
+}
+
+Object.assign(exports, {Handler, tabDeactivate, Reason});
 
 })].map(func => typeof exports == 'undefined' ? require.scopes.reasons = func : func(exports));
