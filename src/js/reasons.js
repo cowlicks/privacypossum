787d6b580ac6bcd4f9cec0ae54a16b17eafad20b
Blake Griffith <cowlicks@riseup.net>
2017-12-01 12:45:28 -0800

    Parameterize tabs & store in Handler

--- src/js/reasons.js
+++ src/js/reasons.js
@@ -12,12 +12,12 @@ function setResponse(response, shortCircuit) {
 
 const tabDeactivate = new Action({response: NO_ACTION, reason: TAB_DEACTIVATE});
 
-function makeReasonMap(tabs) {
+function makeReasonMap() {
   return new Map([
     [FINGERPRINTING, setResponse(CANCEL, false)],
     [USER_URL_DEACTIVATE, setResponse(NO_ACTION, false)],
     [TAB_DEACTIVATE, setResponse(NO_ACTION, true)],
-    [USER_HOST_DEACTIVATE, (details) => {
+    [USER_HOST_DEACTIVATE, (details, {tabs}) => {
       details.shortCircuit = true;
       details.response = NO_ACTION;
       tabs.getTab(details.tabId).action = tabDeactivate;
@@ -27,14 +27,14 @@ function makeReasonMap(tabs) {
 
 // todo wrap handler requests to assure main_frame's are not blocked.
 class Handler {
-  constructor(tabs) {
-    this.tabs = tabs;
-    this.reasonMap = makeReasonMap(tabs);
+  constructor(tabs, store) {
+    Object.assign(this, {tabs, store});
+    this.reasonMap = makeReasonMap();
   }
 
   handleRequest(obj, details) {
     if (obj.hasOwnProperty('action')) {
-      this.reasonMap.get(obj.action.reason)(details);
+      this.reasonMap.get(obj.action.reason)(details, {tabs: this.tabs, store: this.store});
     }
   }
 }
