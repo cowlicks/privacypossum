f6aabf5973e3dce9547dfdf5db9f50ec4fcf17e0
Blake Griffith <cowlicks@riseup.net>
2017-12-01 20:50:11 -0800

    Remove response from schemes_test.js

--- src/js/test/schemes_test.js
+++ src/js/test/schemes_test.js
@@ -12,20 +12,16 @@ function makeAction(response, reason, url, frameUrl, tabUrl) {
 describe('schemes.js', function() {
   describe('Domain', function() {
     describe('#getResponse', function() {
-      it('returns NO_ACTION', function() {
-        let d = new Domain();
-        assert.equal(d.getResponse('foo'), constants.NO_ACTION);
-      });
-      it('gets added Actions', function() {
+      it('gets and sets paths', function() {
         let [k1, k2] = ['path1', 'path2'],
           p1 = new Path(constants.CANCEL, makeAction(...('abcde'.split('')))),
           p2 = new Path(constants.CANCEL, makeAction(...('fghij'.split('')))),
           d = new Domain({paths: {[k1]: p1}});
 
-        assert.equal(d.getResponse(k1), p1.action.response);
+        assert.deepEqual(d.getPath(k1), p1);
 
         d.setPath(k2, p2.action);
-        assert.equal(d.getResponse(k2), p2.action.response);
+        assert.deepEqual(d.getPath(k2), p2);
       });
     });
   });
