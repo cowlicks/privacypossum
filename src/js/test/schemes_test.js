4cf2ca295ad915349a652391f3cea9e3c94c6663
Blake Griffith <cowlicks@riseup.net>
2017-12-02 15:11:15 -0800

    remove response from schemes_test.js

--- src/js/test/schemes_test.js
+++ src/js/test/schemes_test.js
@@ -5,8 +5,8 @@ const assert = require('chai').assert,
   constants = require('../constants');
 
 
-function makeAction(response, reason, url, frameUrl, tabUrl) {
-  return new Action({response, reason, url, frameUrl, tabUrl});
+function makeAction(reason, url, frameUrl, tabUrl) {
+  return new Action({reason, url, frameUrl, tabUrl});
 }
 
 describe('schemes.js', function() {
@@ -14,8 +14,8 @@ describe('schemes.js', function() {
     describe('#getResponse', function() {
       it('gets and sets paths', function() {
         let [k1, k2] = ['path1', 'path2'],
-          p1 = new Path(constants.CANCEL, makeAction(...('abcde'.split('')))),
-          p2 = new Path(constants.CANCEL, makeAction(...('fghij'.split('')))),
+          p1 = new Path(constants.CANCEL, makeAction(...('abcd'.split('')))),
+          p2 = new Path(constants.CANCEL, makeAction(...('fghi'.split('')))),
           d = new Domain({paths: {[k1]: p1}});
 
         assert.deepEqual(d.getPath(k1), p1);
