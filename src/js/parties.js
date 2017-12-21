d74ee108c74508a8fce21e6d09f71d1b3616d60b
Blake Griffith <cowlicks@riseup.net>
2017-12-21 14:22:46 -0600

    Fix bug in isThirdParty

--- src/js/parties.js
+++ src/js/parties.js
@@ -10,9 +10,12 @@ function isThirdParty(d1, d2) {
   let b1 = getBaseDomain(d1),
     b2 = getBaseDomain(d2);
 
-  if (b1 != b2) {
-    return isMdfp(b1, b2);
+  if (b1 == b2 || isMdfp(b1, b2)) {
+    return false;
   }
   return true;
 }
+
+Object.assign(exports, {isThirdParty});
+
 })].map(func => typeof exports == 'undefined' ? require.scopes.parties = func : func(exports));
