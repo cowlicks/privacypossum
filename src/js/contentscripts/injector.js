b7796fb52d0a4d6e3d97eb903834ecddadcadda0
Blake Griffith <cowlicks@riseup.net>
2017-12-21 16:40:00 -0600

    Modify injector to wait for fp script to be ready

--- src/js/contentscripts/injector.js
+++ src/js/contentscripts/injector.js
@@ -4,19 +4,34 @@ let event_id = Math.random();
 
 // listen for messages from the script we are about to insert
 document.addEventListener(event_id, function (e) {
-  chrome.runtime.sendMessage(e.detail);
+  if (e.detail.type === 'fingerprinting') {
+    chrome.runtime.sendMessage(e.detail);
+  }
 });
 
-let s = document.createElement('script');
-s.setAttribute('data', event_id);
-s.src = chrome.extension.getURL('js/web_accessible/fingercounting.js');
-s.onload = function() {
-  this.remove();
-};
-(document.head || document.documentElement).appendChild(s);
+// we wait for the script to say its ready
+let ready = new Promise(resolve => {
+  document.addEventListener(event_id, function (e) {
+    if (e.detail.type === 'ready') {
+      resolve(); // script is ready to receive data
+    }
+  });
+
+  // insert script now that ready listener is listening.
+  let s = document.createElement('script');
+  s.setAttribute('data', event_id);
+  s.src = chrome.extension.getURL('js/web_accessible/fingercounting.js');
+  s.onload = function() {
+    this.remove();
+  };
+  (document.head || document.documentElement).appendChild(s);
+});
+
 
 chrome.runtime.onMessage.addListener(message => {
   if (message.type === 'firstparty-fingerprinting') {
-    document.dispatchEvent(new CustomEvent(event_id, {detail: message}));
+    ready.then(() => {
+      document.dispatchEvent(new CustomEvent(event_id, {detail: message}));
+    });
   }
 });
