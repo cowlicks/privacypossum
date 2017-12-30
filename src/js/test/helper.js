da98df92d88a1240e8e0860386e854bb1eb6c3d9
Blake Griffith <cowlicks@riseup.net>
2017-12-30 10:11:33 -0600

    Add docs to test/helper.js

--- src/js/test/helper.js
+++ src/js/test/helper.js
@@ -1,3 +1,10 @@
+/*
+ * Clear global state between each test
+ *
+ * The node extension api's carry state by design (since browsers do) so we
+ * clear them between each stateful interface between each test.
+ */
+
 const {onConnect, tabsOnMessage, onMessage, tabsQuery} = require('../shim');
 
 beforeEach(function() {
