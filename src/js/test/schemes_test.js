d26c236a8541bca2c58505bbf742b5d7e3cb2314
Blake Griffith <cowlicks@riseup.net>
2018-01-19 11:28:53 -0800

    Use get/set/helper more

--- src/js/test/schemes_test.js
+++ src/js/test/schemes_test.js
@@ -7,23 +7,13 @@ const assert = require('chai').assert,
 describe('schemes.js', function() {
   describe('Domain', function() {
     it('get set update path', async function() {
-      let [[k1, v1], [k2, v2]] = [['k1', 'v1'], ['k2', 'v2']],
-        update = 'update',
-        d = new Domain({paths: {[k1]: v1}});
+      let [initKey1, initVal1] = ['initKey1', 'initVal1'],
+        domain = new Domain({paths: {[initKey1]: initVal1}});
 
-      assert.deepEqual(d.getPath(k1), v1);
+      assert.equal(domain.getPath(initKey1), initVal1);
 
-      d.setPath(k2, v2);
-      assert.deepEqual(d.getPath(k2), v2);
-
-      let before = await new Promise(resolve => {
-        d.updatePath(k2, value => {
-          resolve(value);
-          return update;
-        });
-      });
-      assert.equal(before, v2);
-      assert.equal(d.getPath(k2), update);
+      let [getter, setter, updater] = makeGetterSetterUpdater(domain, 'Path');
+      await getSetUpdateTest(getter, setter, updater);
     });
   });
   it('get/set/updatePathAction', async function() {
@@ -38,18 +28,17 @@ function makeGetterSetterUpdater(obj, suffix) {
 }
 
 async function getSetUpdateTest(getter, setter, updater) {
-  let [[k1, v1], [k2, v2]] = [['k1', 'v1'], ['k2', 'v2']],
-    update = 'update';
+  let [k1, v1] = ['k1', 'v1'], update = 'update';
 
-  setter(k2, v2);
-  assert.deepEqual(getter(k2), v2);
+  setter(k1, v1);
+  assert.deepEqual(getter(k1), v1);
 
   let before = await new Promise(resolve => {
-    updater(k2, value => {
+    updater(k1, value => {
       resolve(value);
       return update;
     });
   });
-  assert.equal(before, v2);
-  assert.equal(getter(k2), update);
+  assert.equal(before, v1);
+  assert.equal(getter(k1), update);
 }
