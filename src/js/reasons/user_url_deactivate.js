14e52e095b36c76bd69beae980df47492180d67b
Blake Griffith <cowlicks@riseup.net>
2018-02-26 19:02:34 -0800

    Add logging to user_url_deactivate reasons

--- src/js/reasons/user_url_deactivate.js
+++ src/js/reasons/user_url_deactivate.js
@@ -3,19 +3,23 @@
 [(function(exports) {
 
 const {USER_URL_DEACTIVATE, NO_ACTION} = require('../constants'),
+    {log} = require('../utils'),
     {setResponse, sendUrlDeactivate} = require('./utils'),
     {Action} = require('../schemes');
 
 async function onUserUrlDeactivate({store, tabs}, {url, tabId}) {
   await store.updateUrl(url, currentAction => {
     let action;
+    log(`got user deactivate message for action: ${currentAction} with url: ${url}`);
     if (currentAction.reason === USER_URL_DEACTIVATE) {
       action = currentAction.getData('deactivatedAction');
+      log(`reactivating action: ${action} for url: ${url}`);
     } else {
       action = new Action(USER_URL_DEACTIVATE, {
         href: url,
         deactivatedAction: currentAction,
       });
+      log(`deactivating action: ${currentAction} for url: ${url}`);
     }
     tabs.markAction(action, url, tabId);
     return action;
