81f1791ed0e620536bf117f5999e71399961b1bc
Blake Griffith <cowlicks@riseup.net>
2018-04-03 15:51:40 -0700

    Add a release.sh script

---
+++ src/release.sh
@@ -0,0 +1,21 @@
+toplevel=$(git rev-parse --show-toplevel)
+src_dir=${toplevel}/src
+manifest=${src_dir}/manifest.json
+today=$(date '+%-d.%-m.%Y')
+out_file=${src_dir}/possum.zip
+
+manifest_version=$(jq ".version" ${manifest})
+if [ ${manifest_version} != "\"${today}\"" ]; then
+    echo "bad version in manifest.json change ${manifest_version} to \"${today}\""
+    exit 1
+fi
+
+echo "tagging version: \"${today}\""
+git tag ${today}
+
+pushd ${toplevel}/src > /dev/null
+
+trap "popd > /dev/null" EXIT
+
+echo "packaging extension to: ${out_file}"
+git ls-files | zip -q ${out_file} -@
