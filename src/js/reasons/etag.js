2d9f8540e34a1a1891629534eed33961a8f1ebe7
Blake Griffith <cowlicks@riseup.net>
2018-04-30 13:31:53 -0700

    Add other etagHeader codepaths and tests

--- src/js/reasons/etag.js
+++ src/js/reasons/etag.js
@@ -5,35 +5,38 @@
 const {etag: {ETAG_TRACKING, ETAG_SAFE, ETAG_UNKNOWN}} = require('../constants'),
     {Action} = require('../schemes');
 
-// if first instance of etag for this url, remove it and store the etag
-// if 2nd time seeing etag for this url, compare new etag with old? if same allow it
-// if it isnt the same mark the url as etag tracking and strip the etag
+// this should just be a method on store
+async function setAction(store, href, reason, data) {
+      return await store.setUrl(href, new Action(reason, data));
+}
+
 function etagHeader({store}, details, header) {
   const {href} = details.urlObj,
     etagValue = header.value,
     action = store.getUrl(href);
   if (action) {
     if (action.reason === ETAG_TRACKING) {
-      // strip header
+      return [true];
     }
     else if (action.reason === ETAG_SAFE) {
       // allow header
+      return [false];
     }
     else if (action.reason === ETAG_UNKNOWN) {
-      if (etagValue === action.etagValue) {
+      if (etagValue === action.data.etagValue) {
         // mark ETAG_SAFE
-        // allow header
+        return [false, setAction(store, href, ETAG_SAFE, {etagValue})];
       } else {
         // mark ETAG_TRACKING
-        // strip header
+        return [true, setAction(store, href, ETAG_TRACKING, {etagValue})];
       }
     }
   } else {
     // mark ETAG_UNKNOWN
-    return [true, store.setUrl(href, new Action(ETAG_UNKNOWN, {etagValue}))];
+    return [true, setAction(store, href, ETAG_UNKNOWN, {etagValue})];
   }
 }
 
-Object.assign(exports, {etagHeader});
+Object.assign(exports, {etagHeader, setAction});
 
 })].map(func => typeof exports == 'undefined' ? define('/reasons/etag', func) : func(exports));
--- src/js/test/reasons/etag_test.js
+++ src/js/test/reasons/etag_test.js
@@ -2,7 +2,7 @@
 
 const {assert} = require('chai'),
   {DomainStore} = require('../../store'),
-  {etagHeader} = require('../../reasons/etag'),
+  {etagHeader, setAction} = require('../../reasons/etag'),
   {etag: {ETAG_TRACKING, ETAG_SAFE, ETAG_UNKNOWN}} = require('../../constants');
 
 describe('etag.js', function() {
@@ -11,16 +11,48 @@ describe('etag.js', function() {
     this.etagHeader = etagHeader.bind(this, this);
   });
   describe('etagHeader', function() {
-    it('marks new etag headers as uknown', async function() {
-      let details = {urlObj: {href: 'https://foo.com/stuff.js'}},
-        header = {name: 'etag', value: 'hi'};
+    let etagValue ='hi', href = 'https://foo.com/stuff.js',
+      details = {urlObj: {href}},
+      header = {name: 'etag', value: etagValue};
 
-      let [removed, done] = this.etagHeader(details, header);
-      await done;
+    it('marks new etag headers as uknown', async function() {
+      let [remove, done] = await this.etagHeader(details, header);
       let action = this.store.getUrl(details.urlObj.href);
-      assert.isTrue(removed);
+      assert.isTrue(remove);
       assert.equal(action.reason, ETAG_UNKNOWN);
       assert.equal(action.data.etagValue, header.value);
     });
+
+    it('allows and marks as safe on same etag', async function() {
+      await setAction(this.store, href, ETAG_UNKNOWN, {etagValue});
+      let [removed] = await this.etagHeader(details, header);
+      assert.isFalse(removed);
+
+      let action = this.store.getUrl(details.urlObj.href);
+      assert.equal(action.reason, ETAG_SAFE);
+      assert.equal(action.data.etagValue, header.value);
+    })
+    it('blocks and marks as tracking on diff etag', async function() {
+      let differentEtagValue = 'different';
+      await setAction(this.store, href, ETAG_UNKNOWN, {etagValue});
+      let [removed] = await this.etagHeader(details, {value: differentEtagValue});
+      assert.isTrue(removed);
+
+      let action = this.store.getUrl(details.urlObj.href);
+      assert.equal(action.reason, ETAG_TRACKING);
+      assert.equal(action.data.etagValue, differentEtagValue);
+    })
+
+    it('blocks etags from tracking urls', async function() {
+      await setAction(this.store, href, ETAG_TRACKING);
+      let [removed] = this.etagHeader(details, header);
+      assert.isTrue(removed);
+    })
+
+    it('allows etags from safe urls', async function() {
+      await setAction(this.store, href, ETAG_SAFE);
+      let [removed] = this.etagHeader(details, header);
+      assert.isFalse(removed);
+    })
   });
 });
