c3fbaee0ed6399bf39ac0fb2e486037b360ad97b
Blake Griffith <cowlicks@riseup.net>
2018-05-18 13:14:57 -0700

    Start tracking etags by host + pathname

--- src/js/reasons/etag.js
+++ src/js/reasons/etag.js
@@ -7,19 +7,20 @@ const {etag: {ETAG_TRACKING, ETAG_SAFE}} = require('../constants'),
   {sendUrlDeactivate} = require('./utils'),
   {Action} = require('../schemes');
 
-async function setEtagAction(store, href, reason, data={etagValue: null}) {
+async function setEtagAction(store, url, reason, data={etagValue: null}) {
     log(`etag update with:
       reason: ${reason}
-      url: ${href}
+      url: ${url}
       etag value: ${data.etagValue}`);
     data.time = Date.now();
-    return await store.setUrl(href, new Action(reason, data));
+    return await store.setUrl(url, new Action(reason, data));
 }
 
 function etagHeader({store, cache}, details, header) {
-  const {href} = details.urlObj,
+  const {protocol, host, pathname} = details.urlObj,
+    url  = `${protocol}//${host}${pathname}`,
     etagValue = header.value,
-    action = store.getUrl(href);
+    action = store.getUrl(url);
   if (action) {
     if (action.reason === ETAG_TRACKING) {
       Object.assign(details, {action})
@@ -28,21 +29,21 @@ function etagHeader({store, cache}, details, header) {
       return false;
     }
   }
-  if (cache.has(href)) {
-    let oldEtagValue = cache.get(href).etagValue;
-    cache.delete(href)
+  if (cache.has(url)) {
+    let oldEtagValue = cache.get(url).etagValue;
+    cache.delete(url)
     if (etagValue === oldEtagValue) {
       // mark ETAG_SAFE
-      setEtagAction(store, href, ETAG_SAFE, {etagValue});
+      setEtagAction(store, url, ETAG_SAFE, {etagValue});
       return false;
     } else {
       // mark ETAG_TRACKING
-      setEtagAction(store, href, ETAG_TRACKING, {etagValue});
+      setEtagAction(store, url, ETAG_TRACKING, {etagValue});
       Object.assign(details, {action: new Action(ETAG_TRACKING, {etagValue})});
       return true;
     }
   } else {
-    cache.set(href, {etagValue});
+    cache.set(url, {etagValue});
     return true;
   }
 }
