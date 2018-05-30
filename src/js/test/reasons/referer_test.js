b3db5c7a7473f19a722df10e7e439f8bb650ff21
Blake Griffith <cowlicks@riseup.net>
2018-05-29 20:08:17 -0700

    Add test/reasons/referer_test.js

---
+++ src/js/test/reasons/referer_test.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const {assert} = require('chai'),
+  {Referer} = require('../../reasons/referer');
+
+describe('referer.js', function() {
+  it('Referer', async function() {
+    const referer = new Referer();
+  });
+});
