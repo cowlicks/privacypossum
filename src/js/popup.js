3113e52f254d411062c5a6bbafaf6e17fe129575
Blake Griffith <cowlicks@riseup.net>
2018-07-12 13:24:30 -0700

    Use popup_component.js in popup.js

--- src/js/popup.js
+++ src/js/popup.js
@@ -9,139 +9,15 @@
 
 [(function(exports) {
 
-let {connect, document, sendMessage, getURL, React, ReactDOM} = require('./shim'),
+let {connect, document, sendMessage, ReactDOM} = require('./shim'),
   {PopupHandler} = require('./reasons/handlers'),
   {View, Counter} = require('./utils'),
   {Action} = require('./schemes'),
+  {popupTitleBar, popupBody} = require('./popup_components'),
   {GET_DEBUG_LOG, POPUP, USER_URL_DEACTIVATE, USER_HOST_DEACTIVATE, HEADER_DEACTIVATE_ON_HOST} = require('./constants');
 
-const enabledText = `ENABLED`,
-    disabledText = `DISABLED`;
-
-const e = React.createElement,
-  eFactory = (name) => (props, ...children) => e(name, props, ...children),
-  span = eFactory('span'),
-  div = eFactory('div'),
-  img = eFactory('img'),
-  code = eFactory('code'),
-  label = eFactory('label'),
-  ul = eFactory('ul'),
-  li = eFactory('li'),
-  input = eFactory('input'),
-  checkbox = (props, ...children) => input(Object.assign({type: 'checkbox'}, props, ...children));
-
-function actionIcon({iconPath, attribution}) {
-  const className = 'action-icon',
-    src = getURL(iconPath);
-  return img({className, src, attribution});
-}
-
-function actionHtml({action, handler, title, iconPath, attribution, url, key}) {
-  const {reason} = action,
-    checked = reason != USER_URL_DEACTIVATE,
-    onChange = handler;
-
-  return li({className: 'action', key},
-    label(
-      {title, ['data-reason']: reason},
-      checkbox({checked, onChange}),
-      actionIcon({action, iconPath, attribution}),
-      url
-    )
-  );
-}
-
-function allActions({actions}) {
-  let list = [];
-
-  Array.from(actions, ([url, actionInfo], index) => {
-    const props = {};
-    Object.assign(props, actionInfo, {url, key: index.toString()});
-    list.push(actionHtml(props));
-  });
-
-  return ul(null, list);
-}
-
-function headerHtml({name, count, key}) {
-  return li({key},
-    code(null, name),
-    ` headers blocked from ${count} sources`
-  );
-}
-
-function headerList({headerCounts}) {
-  let list = [];
-  Array.from(headerCounts, ([name, count], index) => {
-    list.push(headerHtml({name, count, key: index.toString()}));
-  });
-  return ul({id: 'headers-count-list'}, list);
-
-}
-
-function headersSection({headerCounts, onChange, active}) {
-  let children = [];
-  children.push(checkbox({id: 'header-checkbox', checked: active, onChange}));
-  if (active) {
-    children.push('Blocked tracking headers:');
-    children.push(headerList({headerCounts}));
-  } else {
-    children.push('Blocking tracking headers disabled');
-  }
-  return div(null, ...children);
-}
-
-function popupBody({active, urlActions, headerCounts, headerCountsActive, headerHandler}) {
-  let children = [];
-  if (!active) {
-    children.push('Disabled for this site');
-  } else if (urlActions.size === 0 && headerCounts.size === 0 && headerCountsActive) {
-    children.push('Nothing to do');
-  } else {
-    let headerProps = {
-      headerCounts,
-      active: headerCountsActive,
-      onChange: headerHandler,
-    };
-    children.push(div({id: 'headers'},
-      headersSection(headerProps)
-    ));
-    children.push(div({id: 'actions'},
-      allActions({actions: urlActions})
-    ));
-  }
-  return e(React.Fragment, null, ...children);
-}
-
-function popupHeader({onOff, active}) {
-  const onOffElement = e(onOffButton, {onClick: onOff, active});
-  return e(React.Fragment,
-    null,
-    e(branding),
-    onOffElement
-  );
-}
-
-function branding() {
-  return div(
-    {id: 'branding'},
-    img({id: 'logo', src: '/media/popup-icon.png', alt: "I'm a possum"}),
-    span({id: 'title'}, 'Privacy Possum')
-  );
-}
-
-function onOffButton({active, onClick}) {
-  const id = 'on-off',
-    title = `click to ${active ? 'disable' : 'enable'} for this site`;
-
-  return div(
-    {id, onClick, title},
-    span({id: 'on-off-text', className: 'grey'}, active ? enabledText : disabledText),
-    div({id: 'on-off-button'}, img({src: getURL(`/media/logo-${active ? 'active' : 'inactive'}-100.png`)}))
-  );
-}
-
-/*** REACT STUFF ABOVE HERE **/
+const $ = (id) => document.getElementById(id),
+    asyncRender = (component, anchor) => new Promise(resolve => ReactDOM.render(component, anchor, resolve));
 
 class Popup {
   constructor(tabId) {
@@ -149,23 +25,18 @@ class Popup {
     this.handler = new PopupHandler();
     this.tabId = tabId;
 
-    this.getClickHandler = this.handler.getFunc.bind(this.handler);
-
     $('debug-link').onclick = this.debug.bind(this);
 
     this.renderHeader(false);
   }
 
   renderHeader(active) {
-    let ph = popupHeader({onOff: this.onOff.bind(this), active});
-    return new Promise((resolve) => ReactDOM.render(ph, $('title-bar'), resolve));
+    return asyncRender(popupTitleBar({onOff: this.onOff.bind(this), active}), $('title-bar'));
   }
 
-  renderBody() {
-    const {active, urlActions, headerCounts, headerCountsActive} = this,
-      headerHandler = this.headerHandler.bind(this),
-      pb = popupBody({active, urlActions, headerCounts, headerCountsActive, headerHandler});
-    return new Promise((resolve) => ReactDOM.render(pb, $('base'), resolve));
+  renderBody({active, urlActions, headerCounts, headerCountsActive} = this) {
+    let pb = popupBody({active, urlActions, headerCounts, headerCountsActive, headerHandler: this.headerHandler.bind(this)});
+    return asyncRender(pb, $('base'));
   }
 
   connect() {
@@ -188,27 +59,22 @@ class Popup {
     return this.view.ready;
   }
 
-  getActionInfo(action) {
-    let info = this.handler.getInfo(action.reason),
-      title = info.message,
-      iconPath = info.icon, {attribution} = info;
+  getActionInfo(url, action) {
+    action = Action.coerce(action);
+    let {message: title, icon: iconPath, attribution} = this.handler.getInfo(action.reason),
+      handler = this.handler.getFunc(action.reason, [url, this.tabId]);
 
     if (action.reason == USER_URL_DEACTIVATE) {
       let {reason} = action.getData('deactivatedAction');
-      info = this.handler.getInfo(reason);
-      iconPath = info.icon, {attribution} = info;
+      ({icon: iconPath, attribution} = this.handler.getInfo(reason));
     }
-    return {iconPath, title, attribution};
+    return [url, {action, iconPath, title, attribution, handler}];
   }
 
   updateUrlActions(actions) {
-    this.urlActions = new Map();
-    actions.forEach(([url, action]) => {
-      action = Action.coerce(action);
-      const handler = this.getClickHandler(action.reason, [url, this.tabId]),
-        {title, iconPath, attribution} = this.getActionInfo(action);
-      this.urlActions.set(url, {action, handler, title, iconPath, attribution});
-    });
+    this.urlActions = new Map(
+      actions.map(([url, action]) => this.getActionInfo(url, action))
+    );
   }
 
   async onOff() {
@@ -245,11 +111,6 @@ class Popup {
     });
     return out;
   }
-}
-
-
-function $(id) {
-  return document.getElementById(id);
 }
 
 Object.assign(exports, {Popup, $});
