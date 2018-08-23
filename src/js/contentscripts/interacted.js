aee16500fdb1b80b67f683d382def46e23c17bad
Blake Griffith <cowlicks@riseup.net>
2018-08-23 12:15:45 -0700

    Wrap interacted.js into a module.

--- src/js/contentscripts/interacted.js
+++ src/js/contentscripts/interacted.js
@@ -1,5 +1,7 @@
 "use strict";
 
+[(function(exports) {
+
 const INTERACTION = 'interaction',
   clicked = new Set();
 
@@ -7,15 +9,22 @@ function urlToHostname(url) {
   return (new URL(url)).hostname;
 }
 
-document.addEventListener('mousedown', function(e) {
-  if (e.isTrusted) {
-    const hostname = urlToHostname(e.target.baseURI);
-    if (!clicked.has(hostname)) {
-      clicked.add(hostname);
-      chrome.runtime.sendMessage({
-        type: INTERACTION,
-        hostname
-      });
+function initialize() {
+  document.addEventListener('mousedown', function(e) {
+    if (e.isTrusted) {
+      const hostname = urlToHostname(e.target.baseURI);
+      console.log(hostname);
+      if (!clicked.has(hostname)) {
+        clicked.add(hostname);
+        chrome.runtime.sendMessage({
+          type: INTERACTION,
+          hostname
+        });
+      }
     }
-  }
-});
+  });
+}
+
+Object.assign(exports, {initialize});
+
+})].map(func => typeof exports == 'undefined' ? define('/contentscripts/interacted', func) : func(exports));
