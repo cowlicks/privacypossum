dab6ada473a2973d759ebe39ef7c32eab7de3848
Blake Griffith <cowlicks@riseup.net>
2018-08-23 13:43:01 -0700

    Add InteractionWhitelist in interacted.js

--- src/js/reasons/interacted.js
+++ src/js/reasons/interacted.js
@@ -2,12 +2,65 @@
 
 [(function(exports) {
 
-const {INTERACTION} = require('../constants');
+const {INTERACTION} = require('../constants'),
+    {log, LruMap} = require('../utils');
+
+class FifoSet extends Set {
+  constructor(maxSize = 100) {
+    super();
+    Object.assign(this, {maxSize});
+  }
+
+  add(item) {
+    super.add(item);
+    if (this.size > this.maxSize) {
+      this.delete(this.keys().next().value);
+    }
+  }
+}
+
+class InteractionWhitelist {
+  constructor({tabCacheSize=100, hostnameCacheSize=100} = {}) {
+    Object.assign(this, {tabCacheSize, hostnameCacheSize});
+    this.tabs = new LruMap(tabCacheSize);
+  }
+
+  update(tabId, hostname) {
+    if (!this.tabs.has(tabId)) {
+      this.tabs.set(tabId, new FifoSet(this.hostnameCacheSize));
+    }
+    this.tabs.get(tabId).add(hostname);
+    return this;
+  }
+
+  has(tabId, hostname) {
+    if (this.tabs.has(tabId)) {
+      return this.tabs.get(tabId).has(hostname);
+    }
+    return false;
+  }
+}
+
+function wrapTabs(tabs) {
+  const interactionWhiteList = new InteractionWhitelist(),
+    origIsThirdParty = tabs.isThirdParty.bind(tabs),
+    isThirdParty = (tabId, hostname) => {
+      if (interactionWhiteList.has(tabId, hostname)) {
+        return false
+      }
+      return origIsThirdParty(tabId, hostname);
+    };
+  log('wrapping tabs object for interactive whitelist');
+  Object.assign(tabs, {isThirdParty, interactionWhiteList});
+}
 
 function onMessage({tabs}, message, sender) {
   let tabId = sender.tab.id,
     {hostname} = message;
-  tabs.updateInteractionWhitelist(tabId, hostname);
+  if (!tabs.interactionWhiteList) {
+    wrapTabs(tabs);
+  }
+  tabs.interactionWhiteList.update(tabId, hostname);
 }
 
 const reason = {
@@ -17,6 +70,6 @@ const reason = {
   }
 }
 
-Object.assign(exports, {reason});
+Object.assign(exports, {reason, InteractionWhitelist, wrapTabs});
 
 })].map(func => typeof exports == 'undefined' ? define('/reasons/interacted', func) : func(exports));
