aa1b92cf770ade3a15a3291365ada42b55f0b501
Blake Griffith <cowlicks@riseup.net>
2018-09-30 14:16:18 -0700

    make npm_install.sh take a directory argument

--- scripts/npm_install.sh
+++ scripts/npm_install.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 source $(git rev-parse --show-toplevel)/scripts/source_me.sh
 
-pushd ${js_dir} > /dev/null
-trap "popd > /dev/null" EXIT
-
-npm install
+run_in_dir $1 "npm install"
