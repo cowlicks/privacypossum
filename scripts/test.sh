0078af58d16bd8c1eaeabbb311ed73112061316e
Blake Griffith <cowlicks@riseup.net>
2018-09-30 13:55:49 -0700

    Use run_in_dir in unittest script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 source $(git rev-parse --show-toplevel)/scripts/source_me.sh
 
-pushd ${js_dir} > /dev/null
-trap "popd > /dev/null" EXIT
-
-npm test
+run_in_dir $js_dir "npm test"
