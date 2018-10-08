80b6ef5a2227001847f65f50e34498b267ee6a4c
Blake Griffith <blake.a.griffith@gmail.com>
2018-10-07 21:17:17 -0700

    etag test update

--- selenium/etags.js
+++ selenium/etags.js
@@ -1,13 +1,55 @@
+let express = require('express'),
+  vhost = require('vhost'),
+  {firstPartyHostname, thirdPartyHostname, thirdPartyHost, Channel, requestRecorderMiddleware} = require('./utils');
 const ifNoneMatch = 'if-none-match';
-let tracked = new Map();
 
-function etagTrackingApp(request, response) {
-  if request.headers.hasOwnProperty(ifNoneMatch) {
-    let key = request.headers[ifNoneMatch];
-    if tracked.has(key) {
-      let count = tracked.get(key);
-      tracked.set(key, count + 1);
-      response.statusCode = 304
+function etagTracker(app = express()) {
+  let tracked = new Map(),
+    etagCount = 0;
+  app.set('etag', false);
+  Object.assign(app, {tracked, etagCount});
+  app.use((req, res, next) => {
+    if (req.headers.hasOwnProperty(ifNoneMatch) && tracked.has(req.headers[ifNoneMatch])) {
+      let key = req.headers[ifNoneMatch];
+      tracked.set(key, 1 + tracked.get(key));
+      res.statusCode = 304;
+    } else {
+      res.setHeader('etag', `W/"${String(etagCount += 1)}"`);
     }
-  }
+    next();
+  });
+  return app;
 }
+
+function firstPartyApp(app = express(), tpHost = thirdPartyHost) {
+  app.get('/', (req, res) => {
+    return res.send(
+      `<script type="text/javascript" src="http://${tpHost}/tracker.js"></script>`
+    );
+  });
+  return app;
+}
+
+function thirdPartyApp(app = express()) {
+  app.get('/tracker.js', (req, res) => {
+    return res.send('console.log("third party script")');
+  });
+  return app;
+}
+
+function baseTestApp(fpApp, tpApp, app = express(), fpHostname = firstPartyHostname, tpHostname = thirdPartyHostname) {
+  let firstParty = firstPartyApp(fpApp),
+    thirdParty = thirdPartyApp(tpApp);
+  app.use(vhost(fpHostname, firstParty));
+  app.use(vhost(tpHostname, thirdParty));
+  Object.assign(app, {firstParty, thirdParty});
+  return app;
+}
+
+function etagApp() {
+  let fpApp = requestRecorderMiddleware(etagTracker()),
+    tpApp = requestRecorderMiddleware(etagTracker());
+  return baseTestApp(fpApp, tpApp);
+}
+
+Object.assign(module.exports, {etagApp});
