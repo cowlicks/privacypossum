3fe94ab161d48509e461d9c79e3eec9c1eabdaac
Blake Griffith <blake.a.griffith@gmail.com>
2020-08-16 16:29:09 -0700

    rename constants testing

--- src/js/test/testing_constants.js
+++
@@ -1,9 +0,0 @@
-import chai from 'chai'; const {assert} = chai;
-
-import * as constants from '../constants.js';
-
-describe('constants.js', () => {
-  it('has constants attached', () => {
-    assert.equal([...Object.keys(constants)].length > 0, true);
-  });
-});
