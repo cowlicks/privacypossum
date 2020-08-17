cd7da0a78f972cc0598c4bf0889b6127357995df
Blake Griffith <blake.a.griffith@gmail.com>
2020-08-16 23:13:15 -0700

    ESM helpers.js

--- src/js/test/helper.js
+++ src/js/test/helper.js
@@ -7,9 +7,11 @@
  * clear them between each stateful interface between each test.
  */
 
-const {clearState} = require('./testing_utils'),
-  {logger} = require('../utils'),
-  {colors} = require('mocha/lib/reporters/base');
+import {clearState} from './testing_utils.js';
+import {logger} from '../utils.js';
+import mochaBase from 'mocha/lib/reporters/base.js';
+
+const {colors} = mochaBase;
 
 colors['pass'] = '32';
 colors['error stack'] = '31';
